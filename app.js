2788e49b2a4e9b62ad25da55613603b16e1aafb5
siuan <x@siuan.com>
2018-12-06 22:24:52 -0500

    checkpoint DOM 1: completed

---
+++ app.js
@@ -0,0 +1,16 @@
+
+function onReady() {
+  const addToDoForm = document.getElementById('addToDoForm');
+    const newToDoText = document.getElementById('newToDoText');
+    const toDoList = document.getElementById('toDoList');
+
+    addToDoForm.addEventListener();
+     addToDoForm.addEventListener('submit', event => {
+    event.preventDefault();
+  });
+}
+
+window.onload = function() {
+   alert("The window has loaded!");
+    onReady();
+ };
