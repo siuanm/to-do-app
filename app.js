3d43816f8d79de5ec3028513a5a3e7eda61dab1e
siuan <x@siuan.com>
2018-12-14 08:55:25 -0500

    master quick fix

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
 function onReady() {
-  
+
   const addToDoForm = document.getElementById('addToDoForm');
     const newToDoText = document.getElementById('newToDoText');
     const toDoList = document.getElementById('toDoList');
 
 
-    addToDoForm.addEventListener();
+  
      addToDoForm.addEventListener('submit', event => {
     event.preventDefault();
 
